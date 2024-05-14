27e9ea86bf12881a1b28bcb976c10dfc220bebef
Chloe Harrison <chloesian.harrison@live.co.uk>
2024-05-14 13:03:31 +0100

    basic added and index quest created

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -1,2 +1,26 @@
 class QuestsController < ApplicationController
+  def index
+    @quests = Quest.where(Quest[:user] == current_user[:id])
+  end
+
+  def new
+    @quest = Quest.new
+  end
+
+  def create
+    @quest = Quest.new(quest_params)
+    @quest.user = current_user
+    quest.xp = ((@quest.fun_rating + @quest.difficulty_rating + @quest.consequence_level) / 3) * 10.floor
+    if @quest.save
+      redirect_to quests_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def quest_params
+    params.require(:quest).permit(:name, :notes, :due_date, :fun_rating, :difficulty_rating, :consequence_level, :completed)
+  end
 end
