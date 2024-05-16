0d1067a4ed6d3ee99d2d2b09132c738198918693
Emma Fumeaux <emma.fumeaux@gmail.com>
2024-05-16 11:42:14 +0100

    seeds file

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -2,7 +2,7 @@ class RewardsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @rewards = Reward.all
+    @rewards = Reward.where(user_id: current_user[:id])
   end
 
   def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,47 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+puts "Cleaning users..."
+User.destroy_all
+puts "Cleaning quests..."
+Quest.destroy_all
+puts "Cleaning rewards..."
+Reward.destroy_all
+
+puts 'Creating new database...'
+
+3.times do
+  user = User.create(
+    email: Faker::Internet.email,
+    username: Faker::Internet.username,
+    password: "123456",
+    xp: Faker::Number.within(range: 10..120)
+  )
+  puts 'User created.'
+
+  3.times do
+    user.quests.create(
+      name: Faker::Hobby.activity,
+      xp: Faker::Number.within(range: 10..80),
+      notes: Faker::Lorem.paragraph(sentence_count: 2),
+      due_date: Faker::Date.between(from: '2024-05-23', to: '2026-12-31'),
+      fun_rating: Faker::Number.within(range: 1..10),
+      difficulty_rating: Faker::Number.within(range: 1..10),
+      consequence_level: Faker::Number.within(range: 1..10),
+      completed: Faker::Boolean.boolean
+    )
+    puts 'Quest created.'
+
+    user.rewards.create(
+      name: Faker::Hobby.activity,
+      xp: Faker::Number.number(digits: 3),
+      details: Faker::Lorem.paragraph(sentence_count: 2),
+      completed: Faker::Boolean.boolean
+    )
+    puts 'Reward created.'
+  end
+end
+puts 'Database created.'
