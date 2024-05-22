501c7b72527d347d0dd241d03eeb7c02932b2a09
Haris Yousaf haris__yousaf@hotmail.com <haris__yousaf@hotmail.com>
2024-05-22 16:45:33 +0100

    xp update

--- app/javascript/controllers/bubble_controller.js
+++ app/javascript/controllers/bubble_controller.js
@@ -2,6 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="bubble"
 export default class extends Controller {
+  static values = {
+    token: String,
+    userid: String
+  }
   connect() {
     let xpCount = 0;
     const counter = document.querySelector("#xpCounter");
@@ -20,6 +24,24 @@ export default class extends Controller {
       bubble.style.left = '';
     };
 
+    const updateXp = (user_id, xp) => {
+      const body = {user: {xp: xp}};
+      fetch(`/users/${user_id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+           "Accept": "application/json",
+           "X-CSRF-Token": this.tokenValue
+        },
+        body: JSON.stringify(body)
+      })
+        .then(response => response.json())
+        .then((data) => {
+          console.log(data)
+        })
+    };
+
+
     const pop = new Audio('http://contentservice.mc.reyrey.net/audio_v1.0.0/?id=e049b733-1543-51fd-9ce9-680f57226aa1')
 
     bubbles.forEach(bubble => {
@@ -28,6 +50,7 @@ export default class extends Controller {
           xpCount += 1;
           pop.play()
           counter.innerText = `xp: ${xpCount}`;
+          updateXp(this.useridValue, 1)
           if (xpCount === 10) {
             window.location.href = "/quests";
           }
